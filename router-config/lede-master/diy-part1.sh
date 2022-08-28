35b1de07be57dc1f4b913fadd6224550a677297a
jackie099 <jackie099@me.com>
2022-08-28 07:07:07 -0400

    Update diy-part1.sh

--- router-config/lede-master/diy-part1.sh
+++ router-config/lede-master/diy-part1.sh
@@ -16,3 +16,5 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
